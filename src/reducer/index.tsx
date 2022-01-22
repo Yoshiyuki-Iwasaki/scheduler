cdb976c1c8b6589ea8fb73bbf159dae4c4c1115f
Yoshiyuki Iwasaki <armada3524@gmail.com>
2022-01-22 14:51:48 +0900

    fix type.

--- src/reducer/index.tsx
+++ src/reducer/index.tsx
@@ -1,9 +1,9 @@
-const events = (state: any = [], action: any) => {
+const posts = (state: any[] = [], action: any) => {
   switch (action.type) {
     case 'CREATE_EVENT':
       const event: any = { title: action.title, body: action.body };
-      const length: any = state.length;
-      const id: any = length === 0 ? 1 : state[length - 1].id + 1;
+      const length: number = state.length;
+      const id: number = length === 0 ? 1 : state[length - 1].id + 1;
       return [...state, { id, ...event }];
     case 'DELETE_EVENT':
       return state.filter((event: any) => event.id !== action.id);
@@ -14,4 +14,4 @@ const events = (state: any = [], action: any) => {
   }
 };
 
-export default events;
+export default posts;
